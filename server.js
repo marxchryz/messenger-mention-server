e10a4664ad1fa9a80c8d9b66d56cb032a34d2de9
Marx Chryz <marxchryz30@gmail.com>
2021-05-28 19:33:25 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const login = require('facebook-chat-api');
+const cors = require('cors');
+const app = express();
+const PORT = 5002;
+
+app.use(cors());
+app.use(express.json());
+app.listen(PORT, () => {
+  console.log('App started at port ' + PORT);
+});
+
+app.post('/mention/:threadId', (req, res) => {
+  const threadId = req.params.threadId;
+  let { message = '@everyone', state } = req.body;
+  message = message.length ? message : '@everyone';
+  state = JSON.parse(state);
+
+  login({ appState: state }, (err, api) => {
+    if (err)
+      return res.json({
+        failed: true,
+      });
+
+    var msgToSend = {
+      body: '\u200e' + message,
+      mentions: [],
+    };
+
+    api.getThreadInfo(threadId, (err, data) => {
+      data.participantIDs.map((id) => {
+        if (id !== api.getCurrentUserID())
+          msgToSend.mentions.push({ tag: message, id });
+      });
+
+      api.sendMessage(msgToSend, threadId, (err) => {
+        if (err)
+          return res.json({
+            failed: true,
+          });
+        return res.json({
+          success: true,
+        });
+      });
+    });
+  });
+});
